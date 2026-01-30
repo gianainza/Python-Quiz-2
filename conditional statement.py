44aa3f084b3aa8897d4a1e41d8ec899fddb98bb5
gianainza <ainzagian@gmail.com>
2026-01-30 15:46:42 +0800

    CONDITIONAL STATEMENT CODE

---
+++ conditional statement.py
@@ -0,0 +1,11 @@
+# 2. Conditional Statement
+print("Conditional Statement\n")
+
+number = int(input("Enter a number: "))
+ 
+if number > 0:
+    print("The number is Positive.\n")
+elif number < 0:
+    print("The number is Negative.\n")
+else:
+    print("The number is Zero.\n")
